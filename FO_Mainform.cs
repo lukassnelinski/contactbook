b9729fbc63f26ccf1970febd0e4acdc24a03389c
Lukas Snelinski <hello@lukas.snelinski.at>
2021-02-14 22:46:23 +0100

    Initial commit

---
+++ FO_Mainform.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Contactbook
+{
+    public partial class FO_Mainform : Form
+    {
+        public FO_Mainform()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Contactbook
+
+This is a little contactbook. It's using C# and SQLite.
